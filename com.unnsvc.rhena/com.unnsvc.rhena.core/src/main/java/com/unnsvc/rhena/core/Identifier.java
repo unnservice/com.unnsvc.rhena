318636f31b499940ac795d44fc81508bb61aaa0d
Paul Alesius <paul@unnservice.com>
2016-09-27 11:58:47 +0200

    remove target directory

--- com.unnsvc.rhena/com.unnsvc.rhena.core/src/main/java/com/unnsvc/rhena/core/Identifier.java
+++ com.unnsvc.rhena/com.unnsvc.rhena.core/src/main/java/com/unnsvc/rhena/core/Identifier.java
@@ -6,4 +6,8 @@ public class Identifier {
 
 	public static final String IDENTIFIER = "[a-Z0-9](\\.[a-Z0-9])*";
 	public static final Pattern IDENTIFIER_PATTERN = Pattern.compile(IDENTIFIER);
+	
+	public Identifier(String identifier) {
+		
+	}
 }
