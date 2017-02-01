ba86414d8a49e797c6165d48cbfc32462a1cfdbe
Paul Alesius <paul@unnservice.com>
2017-02-01 22:58:15 +0100

    Tests run and remove superfluous code

--- com.unnsvc.rhena.cmdline/src/test/java/com/unnsvc/rhena/cmdline/TestRhenaModule.java
+++ com.unnsvc.rhena.cmdline/src/test/java/com/unnsvc/rhena/cmdline/TestRhenaModule.java
@@ -6,80 +6,41 @@ import java.io.File;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.unnsvc.rhena.common.IRhenaConfiguration;
-import com.unnsvc.rhena.common.IRhenaContext;
 import com.unnsvc.rhena.common.IRhenaEngine;
-import com.unnsvc.rhena.common.exceptions.RhenaException;
 import com.unnsvc.rhena.common.execution.EExecutionType;
 import com.unnsvc.rhena.common.execution.IRhenaExecution;
 import com.unnsvc.rhena.common.identity.ModuleIdentifier;
 import com.unnsvc.rhena.common.model.IRhenaModule;
+import com.unnsvc.rhena.core.AbstractRhenaTest;
 import com.unnsvc.rhena.core.Caller;
-import com.unnsvc.rhena.core.RhenaConfiguration;
-import com.unnsvc.rhena.core.RhenaContext;
 import com.unnsvc.rhena.core.RhenaEngine;
 import com.unnsvc.rhena.core.execution.WorkspaceExecution;
-import com.unnsvc.rhena.core.logging.SystemOutLogListener;
-import com.unnsvc.rhena.core.resolution.LocalCacheRepository;
 import com.unnsvc.rhena.core.resolution.WorkspaceRepository;
-import com.unnsvc.rhena.core.visitors.DebugModelVisitor;
 
-public class TestRhenaModule {
-
+/**
+ * Basically the same test as in core for now
+ * @author noname
+ *
+ */
+public class TestRhenaModule extends AbstractRhenaTest {
+	
 	@Test
 	public void testModule() throws Exception {
 
-		IRhenaConfiguration config = new RhenaConfiguration();
-		config.setRhenaHome(new File(System.getProperty("user.home"), ".rhena"));
-		config.setParallel(false);
-		// Produce packages or use exploded compilation
-		config.setPackageWorkspace(false);
-		config.setInstallLocal(true);
-		// context.setLogHandler(IRhenaLogHandler logHandler);
-		// context.getRepositoryConfiguration().addRepository()
-		// context.getRepositoryConfiguration().setProxyXX?
-		// context.getTestConfiguration().setXXX
-		// context.addListener...
+		getContext().addWorkspaceRepository(new WorkspaceRepository(getContext(), new File("../../")));
+		getContext().addWorkspaceRepository(new WorkspaceRepository(getContext(), new File("../")));
 
-		/**
-		 * This portion below can be executed multiple times, make sure there
-		 * are no resource leaks in the lifecycles
-		 */
-		try (IRhenaContext context = new RhenaContext(config)) {
-			context.addWorkspaceRepository(new WorkspaceRepository(context, new File("../../")));
-			context.addWorkspaceRepository(new WorkspaceRepository(context, new File("../")));
-			context.setLocalRepository(new LocalCacheRepository(context));
-			context.getListenerConfig().addListener(new SystemOutLogListener());
+		IRhenaEngine engine = new RhenaEngine(getContext());
 
-			IRhenaEngine engine = new RhenaEngine(context);
+		IRhenaModule entryPointModule = engine.materialiseModel(ModuleIdentifier.valueOf("com.unnsvc.rhena:core:0.0.1"));
+		Assert.assertNotNull(entryPointModule);
 
-			IRhenaModule entryPointModule = engine.materialiseModel(ModuleIdentifier.valueOf("com.unnsvc.rhena:core:0.0.1"));
-			Assert.assertNotNull(entryPointModule);
+		debugContext(engine);
 
-			debugContext(engine);
+		IRhenaExecution execution = engine.materialiseExecution(new Caller(entryPointModule, EExecutionType.TEST));
+		Assert.assertNotNull(execution);
 
-			IRhenaExecution execution = engine.materialiseExecution(new Caller(entryPointModule, EExecutionType.TEST));
-			Assert.assertNotNull(execution);
-
-			// because we want to configure eclipse with it
-			Assert.assertTrue(execution instanceof WorkspaceExecution);
-
-			// introspect model?
-		} catch (RhenaException ex) {
-
-			throw new Exception(ex);
-		}
-	}
-
-	private void debugContext(IRhenaEngine engine) throws RhenaException {
-
-		engine.getContext().getCache().getModules().forEach((identifier, module) -> {
-			try {
-				module.visit(new DebugModelVisitor(engine.getContext(), 0, engine));
-			} catch (RhenaException e) {
-
-				e.printStackTrace();
-			}
-		});
+		// because we want to configure eclipse with it
+		Assert.assertTrue(execution instanceof WorkspaceExecution);
 	}
 }
