98ec6a1397db2ca0f8cd258593e60e0dc5af448a
Paul Alesius <paul@unnservice.com>
2017-04-21 20:39:16 +0200

    Agent client provides an interface

--- com.unnsvc.rhena.agent/src/main/java/com/unnsvc/rhena/agent/RhenaAgentClient.java
+++ com.unnsvc.rhena.agent/src/main/java/com/unnsvc/rhena/agent/RhenaAgentClient.java
@@ -1,13 +1,15 @@
+
 package com.unnsvc.rhena.agent;
 
 import java.net.SocketAddress;
 
+import com.unnsvc.rhena.common.IRhenaAgentClient;
 import com.unnsvc.rhena.common.execution.IExecutionRequest;
 import com.unnsvc.rhena.common.execution.IExecutionResult;
 import com.unnsvc.rhena.objectserver.ObjectServerException;
 import com.unnsvc.rhena.objectserver.client.ObjectClient;
 
-public class RhenaAgentClient extends ObjectClient<IExecutionRequest, IExecutionResult> {
+public class RhenaAgentClient extends ObjectClient<IExecutionRequest, IExecutionResult> implements IRhenaAgentClient {
 
 	public RhenaAgentClient(SocketAddress socketAddress) throws ObjectServerException {
 
---
+++ com.unnsvc.rhena.common/src/main/java/com/unnsvc/rhena/common/IRhenaAgentClient.java
@@ -0,0 +1,10 @@
+
+package com.unnsvc.rhena.common;
+
+import com.unnsvc.rhena.common.execution.IExecutionRequest;
+import com.unnsvc.rhena.common.execution.IExecutionResult;
+import com.unnsvc.rhena.objectserver.IObjectClient;
+
+public interface IRhenaAgentClient extends IObjectClient<IExecutionRequest, IExecutionResult> {
+
+}
