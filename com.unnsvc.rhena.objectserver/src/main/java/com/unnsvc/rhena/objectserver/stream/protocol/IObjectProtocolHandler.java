91fcf71d0165547a763bdc59bb014666910cfbde
Paul Alesius <paul@unnservice.com>
2017-06-18 17:41:40 +0200

    Protocol handler throws protocol exception

--- com.unnsvc.rhena.objectserver/src/main/java/com/unnsvc/rhena/objectserver/stream/protocol/IObjectProtocolHandler.java
+++ com.unnsvc.rhena.objectserver/src/main/java/com/unnsvc/rhena/objectserver/stream/protocol/IObjectProtocolHandler.java
@@ -1,11 +1,12 @@
 
 package com.unnsvc.rhena.objectserver.stream.protocol;
 
+import com.unnsvc.rhena.objectserver.stream.ConnectionProtocolException;
 import com.unnsvc.rhena.objectserver.stream.messaging.IRequest;
 import com.unnsvc.rhena.objectserver.stream.messaging.IResponse;
 
 public interface IObjectProtocolHandler {
 
-	public IResponse handleRequest(IRequest request) throws Exception;
+	public IResponse handleRequest(IRequest request) throws ConnectionProtocolException;
 
 }
--- com.unnsvc.rhena.objectserver/src/test/java/com/unnsvc/rhena/objectserver/stream/misc/ObjectProtocolHandler.java
+++ com.unnsvc.rhena.objectserver/src/test/java/com/unnsvc/rhena/objectserver/stream/misc/ObjectProtocolHandler.java
@@ -4,6 +4,7 @@ package com.unnsvc.rhena.objectserver.stream.misc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.unnsvc.rhena.objectserver.stream.ConnectionProtocolException;
 import com.unnsvc.rhena.objectserver.stream.messaging.IRequest;
 import com.unnsvc.rhena.objectserver.stream.messaging.IResponse;
 import com.unnsvc.rhena.objectserver.stream.protocol.IObjectProtocolHandler;
@@ -13,7 +14,7 @@ public class ObjectProtocolHandler implements IObjectProtocolHandler {
 	private Logger log = LoggerFactory.getLogger(getClass());
 
 	@Override
-	public IResponse handleRequest(IRequest request) throws Exception {
+	public IResponse handleRequest(IRequest request) throws ConnectionProtocolException {
 
 		log.info("Replying to request: " + request);
 		return new SuccessfulResponse();
